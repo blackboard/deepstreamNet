f41eb44d5bcc65dfa1ed2e3e897e66ddc463dda2
Tomas Paseka <tomas.paseka@blackboard.com>
2017-10-13 12:39:15 +0200

    - added DeepStream Connection - added record tests

---
+++ UWPTest/DeepStream/Connection.cs
@@ -0,0 +1,113 @@
+﻿using DeepStreamNet;
+using DeepStreamNet.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPTest.DeepStream
+{
+    class Connection
+    {
+        static private Connection _instance;
+        static private Object _instanceLock = new Object();
+        static readonly String NoError = "no error";
+
+        private DeepStreamClient _client;
+        private String _lastError = NoError;
+
+        static public Connection Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_instanceLock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Connection();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private Connection()
+        {
+        }
+
+        private void ResetLastError()
+        {
+            _lastError = NoError;
+        }
+
+        public async Task<bool> ConnectAsync(String host, int port)
+        {
+            bool result = false;
+            ResetLastError();
+            Disconnect();
+
+            try
+            {
+                _client = new DeepStreamClient(host, port, "deepstream", false);
+                result = await _client.LoginAsync();
+            } 
+            catch (Exception ex)
+            {
+                _lastError = ex.Message;
+            }
+
+            return result;
+        }
+
+        public async Task<IDeepStreamRecord> GetRecordAsync(String name)
+        {
+            ResetLastError();
+
+            IDeepStreamRecord result = null;
+            if (_client != null)
+            {
+                try
+                {
+                    if (await _client.Records.HasAsync(name))
+                    {
+                        result = await _client.Records.GetRecordAsync(name);
+                    }
+                    else
+                    {
+                        _lastError = "Record not exists";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _lastError = ex.Message;
+                }
+            }
+            else
+            {
+                _lastError = "Not Connected";
+            }
+
+            return result;
+        }
+
+        public String LastError
+        {
+            get
+            {
+                return _lastError;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (_client != null)
+            {
+                _client.Dispose();
+            }
+        }
+    }
+}
--- UWPTest/MainPage.xaml.cs
+++ UWPTest/MainPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using DeepStreamNet;
+using DeepStreamNet.Contracts;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using UWPTest.DeepStream;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,7 +30,67 @@ namespace UWPTest
         {
             this.InitializeComponent();
 
-            var client = new DeepStreamClient("localhost", 6020, "deepstream");
+            
         }
+
+        private async void btnConnect_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            // TODO: This should be configurable
+            //String host = "deepstream-dev.bbpd.io";
+            String host = "10.103.66.54"; // VPN for phone doesn't resolve host name
+            int port = 6020;
+
+            AppendToOutput("Connecting..");
+            bool result = await Connection.Instance.ConnectAsync(host, port);
+            if (result)
+            {
+                AppendToOutput(String.Format("Connected to: {0}:{1}", host, port));
+            }
+            else
+            {
+                AppendToOutput("Failed to connect: " + Connection.Instance.LastError);
+            }
+        }
+
+        private void AppendToOutput(String str)
+        {
+            txtOutput.Text += "\n" + str;
+        }
+
+        private void dumpRecord(IDeepStreamRecord record)
+        {
+            AppendToOutput(String.Format("Record: {0} {1}", record.RecordName, record.ToString()));            
+        }
+
+        private async void btnRecordRead_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            IDeepStreamRecord record = await Connection.Instance.GetRecordAsync(txtRecordName.Text);
+
+            if (record != null)
+            {
+                dumpRecord(record);
+            }
+            else
+            {
+                AppendToOutput(String.Format("Failed to get record: {0}", Connection.Instance.LastError));
+            }
+        }
+
+        private async void btnRecordWrite_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            IDeepStreamRecord record = await Connection.Instance.GetRecordAsync(txtRecordName.Text);
+
+            if (record != null)
+            {
+                record["detail"] = int.Parse(txtRecordData.Text);
+                AppendToOutput("Record updated.");
+            }
+            else
+            {
+                AppendToOutput(String.Format("Failed to get record: {0}", Connection.Instance.LastError));
+            }
+        }
+
+        //        private updateUI
     }
 }
